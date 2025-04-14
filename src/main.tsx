8c8c845a2fe6653bc083004ae35e209381f721e4
Lucas Panizio <lucas_panizio@hotmail.com>
2025-04-14 14:26:11 -0300

    :wrench: chore: add global style and remove global.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Calculator } from "@/components/Calculator/index.tsx";
-import { AppProvider } from "@/store/hooks/useApp.tsx";
-import "./global.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import isPropValid from '@emotion/is-prop-valid';
+import { StyleSheetManager } from 'styled-components';
+import { Calculator } from '@/components/calculator';
+import { AppProvider } from '@/store/app-provider';
+import { GlobalStyle } from '@/global/styles';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppProvider>
-      <Calculator />
+      <StyleSheetManager enableVendorPrefixes shouldForwardProp={isPropValid}>
+        <GlobalStyle />
+        <Calculator />
+      </StyleSheetManager>
     </AppProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
